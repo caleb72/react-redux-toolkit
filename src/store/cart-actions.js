8b14e2dabead8f6888428ffe264df0c70072fa58
Patrick Castle <patrick.c.castle@gmail.com>
2023-01-28 14:52:56 +0000

    End of section 19

---
+++ src/store/cart-actions.js
@@ -0,0 +1,89 @@
+import { uiActions } from "./ui";
+import { cartActions } from "./cart";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://vicroads-assistant.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+
+      const data = await response.json();
+
+      return (
+        data || {
+          items: [],
+          totalQuantity: 0,
+          totalAmount: 0.0,
+        }
+      );
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: error.message,
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://vicroads-assistant.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+            totalAmount: cart.totalAmount,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully",
+        })
+      );
+    };
+
+    try {
+      await sendRequest();
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: error.message,
+        })
+      );
+    }
+  };
+};
--- src/store/cart.js
+++ src/store/cart.js
@@ -3,37 +3,46 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   items: [],
   totalQuantity: 0,
-  totalAmount: 0.0
+  totalAmount: 0.0,
+  isChanged: false,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    replaceCart: (state, action) => {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.totalAmount = action.payload.totalAmount;
+      state.items = action.payload.items;
+      state.isChanged = false;
+    },
     addItem: (state, action) => {
       const newItem = action.payload;
 
-      const existingItem = state.items.find(
-        (item) => item.itemId === newItem.id
-      );
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           title: newItem.title,
           price: newItem.price,
           totalPrice: newItem.price,
-          quantity: 1
+          quantity: 1,
         });
       } else {
         existingItem.quantity++;
         existingItem.totalPrice += newItem.price;
       }
+
+      state.totalQuantity++;
+      state.totalAmount += newItem.price;
+      state.isChanged = true;
     },
 
     removeItem: (state, action) => {
       const id = action.payload;
 
-      const existingIndex = state.items.findIndex((item) => item.itemId === id);
+      const existingIndex = state.items.findIndex((item) => item.id === id);
       const existingItem = state.items[existingIndex];
 
       if (existingItem.quantity > 1) {
@@ -42,12 +51,16 @@ const cartSlice = createSlice({
       } else {
         state.items.splice(existingIndex, 1);
       }
+
+      state.totalQuantity--;
+      state.totalAmount -= existingItem.price;
+      state.isChanged = true;
     },
 
     emptyCart: (state) => {
       state = initialState;
-    }
-  }
+    },
+  },
 });
 
 export const cartActions = cartSlice.actions;
